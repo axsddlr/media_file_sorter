267029b582b8865c63f7a62c0c62c81080ad8996
Andre Saddler <dresadd09691@gmail.com>
2022-06-29 08:57:06 -0400

    main: use get_videos for get_files arg

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import argparse
 
-from util.utils import move_small, move_large, get_file_sizes
+from util.utils import move_small, move_large, get_file_sizes, get_videos
 
 if __name__ == '__main__':
     # create an argument parser
     parser = argparse.ArgumentParser()
-    # add an argument for the path
     parser.add_argument('path', help='the path to search')
-    # add and argument for move_small function
     parser.add_argument('--move_small', help='move files to small files directory', action='store_true')
     parser.add_argument('--move_large', help='move files to large files directory', action='store_true')
     parser.add_argument('--get_file_sizes', help='get file sizes', action='store_true')
-    # add and argument for move_large with a default value of 100
     parser.add_argument('--size', help='the size to move files to', type=int)
+    parser.add_argument('--get_files', help='get files', action='store_true')
     args = parser.parse_args()
     # get the path
     path = args.path
@@ -31,9 +29,10 @@ if __name__ == '__main__':
         move_large(path, args.size)
     elif args.get_file_sizes:
         print(get_file_sizes(path))
+    elif args.get_files:
+        print(get_videos(path))
     else:
         print('No arguments passed')
         print('Please use --help for more information')
         print('Exiting...')
         exit()
-
